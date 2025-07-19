3d6f081b8ef2f2d5a5310df67d32cc7db76728de
Nivando Soares <sidvandoni@gmail.com>
2025-07-19 09:10:24 -0300

    title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Meu progresso em Análise e desenvolvimento de sistemas',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
